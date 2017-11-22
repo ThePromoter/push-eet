1822884ae90472d725525057ba274b539056bf73
ThePromoter <pinciotti.dan182@gmail.com>
2017-11-22 01:16:01 -0500

    Handles push notification that uses data map

--- app/src/main/java/com/pusheet/app/MainActivity.java
+++ app/src/main/java/com/pusheet/app/MainActivity.java
@@ -2,9 +2,8 @@ package com.pusheet.app;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.provider.Settings;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
--- app/src/main/java/com/pusheet/app/MyFirebaseMessagingService.java
+++ app/src/main/java/com/pusheet/app/MyFirebaseMessagingService.java
@@ -1,13 +1,8 @@
 package com.pusheet.app;
 
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.support.v4.app.NotificationCompat;
-
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.google.firebase.messaging.RemoteMessage.Notification;
 
 import org.greenrobot.eventbus.EventBus;
 
@@ -19,6 +14,13 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     @Override public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
-        EventBus.getDefault().post(new NewNotificationEvent(remoteMessage.getNotification().getBody()));
+        Notification notification = remoteMessage.getNotification();
+        final String message;
+        if (notification != null) {
+            message = notification.getBody();
+        } else {
+            message = remoteMessage.getData().get("payload");
+        }
+        EventBus.getDefault().post(new NewNotificationEvent(message));
     }
 }
